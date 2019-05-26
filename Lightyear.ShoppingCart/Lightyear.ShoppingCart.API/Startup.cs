b9f28ed1283234300aed20c149bb4b9ce1c3a6a9
Jackson Diógenes Neves Mourão <rasert@hotmail.com>
2019-05-26 16:35:05 -0300

    Adicionado o swagger na API ShoppingCart.

--- Lightyear.ShoppingCart/Lightyear.ShoppingCart.API/Startup.cs
+++ Lightyear.ShoppingCart/Lightyear.ShoppingCart.API/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Lightyear.ShoppingCart.API
 {
@@ -23,7 +24,14 @@ namespace Lightyear.ShoppingCart.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHealthChecks();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Lightyear ShoppingCart API", Version = "v1" });
+            });
 
             services.AddStackExchangeRedisCache(options =>
             {
@@ -69,6 +77,16 @@ namespace Lightyear.ShoppingCart.API
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lightyear ShoppingCart API V1");
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
